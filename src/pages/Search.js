dcba87a3d592e04a4faaf7de37660f0111471cb7
Yeop <rlaxoduq001@gmail.com>
2023-12-16 14:53:01 +0900

    input 반응형 수정

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -8,15 +8,15 @@ export const Search = () => {
       height: "200px",
       textAlign: "center"
     }}>
-      
       <input style={{
             display: "block",
             flex: "1",
             fontSize: "80px",
             margin: "auto",
+            maxWidth: "100%", // 최대 너비 설정
+            width: "100%",    // 너비를 부모 요소에 맞춤
       }}></input>
-
-
+  
     </div>
   )
 }
